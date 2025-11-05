64ffc51a6c4e1db7d3782a637f38f484e7363c27
Damon Strickland <damonstrick@users.noreply.github.com>
2025-11-05 17:27:51 -0500

    Add permissions overview page

---
+++ app/permissions/page.tsx
@@ -0,0 +1,10 @@
+import MainContent from '@/components/MainContent';
+
+export default function PermissionsPage() {
+  return (
+    <div className="w-full">
+      <MainContent />
+    </div>
+  );
+}
+
