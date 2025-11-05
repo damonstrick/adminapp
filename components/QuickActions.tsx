6bbce8cd8815d6da55c383d09f7d90ad39c64025
Damon Strickland <damonstrick@users.noreply.github.com>
2025-11-05 17:35:45 -0500

    Reduce noise overlay opacity by 50% in quick action cards

--- components/QuickActions.tsx
+++ components/QuickActions.tsx
@@ -140,7 +140,7 @@ function QuickActionCard({ icon, title, description, circleColor, onClick }: { i
             patternScaleX={1}
             patternScaleY={1}
             patternRefreshInterval={2}
-            patternAlpha={25}
+            patternAlpha={12.5}
             blendMode="color-burn"
           />
         </div>
